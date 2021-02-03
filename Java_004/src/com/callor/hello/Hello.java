8be9073aa5400248fda2c3999f5a0b566cff9b24
p01076322603 <p01076322603@gmail.com>
2021-02-03 09:30:37 +0900

    README 파일 수정

--- Java_004/src/com/callor/hello/Hello.java
+++ Java_004/src/com/callor/hello/Hello.java
@@ -1,7 +1,17 @@
 package com.callor.hello;
 
 public class Hello {
+
 	public static void main(String[] args) {
+		
+		int num1 = 55;
+		int num2 = 68;
+				
+		System.out.println( num1 + num2 );
+		System.out.println( num1 * num2 );
+		System.out.println( num2 - num1 );
+		System.out.println( num2 / num1 );
+		System.out.println( num2 % num1 );
 		
 	}
 
