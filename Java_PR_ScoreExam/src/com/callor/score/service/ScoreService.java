e9de978a13fa12213e35f7893305cea88756b9ba
p01076322603 <p01076322603@gmail.com>
2021-03-10 16:14:11 +0900

    PR

---
+++ Java_PR_ScoreExam/src/com/callor/score/service/ScoreService.java
@@ -0,0 +1,13 @@
+
+package com.callor.score.service;
+
+import com.callor.score.model.ScoreVO;
+
+public interface ScoreService {
+    
+    public void saveScoreToList();
+    public void makeScoreToFile();
+    public void printScoreList();
+    public ScoreVO makeScoreAvgTotal(ScoreVO score);
+
+}
